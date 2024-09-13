ee12e9acf6f5ddde8e80fb7ff36be5859e3880f2
Michalis Toutoudakis <miketoutou@gmail.com>
2024-09-13 15:11:56 +0300

    Update dockerActions.groovy

--- vars/dockerActions.groovy
+++ vars/dockerActions.groovy
@@ -1,11 +1,10 @@
 def buildDockerImage(String imageName, String registryDomain) {
-    container('docker') {
-        script {
-            withCredentials([usernamePassword(credentialsId: 'nexus', usernameVariable: 'NEXUS_USERNAME', passwordVariable: 'NEXUS_PASSWORD')]) {
-                sh "echo $NEXUS_PASSWORD | docker login ${registryDomain} -u ${NEXUS_USERNAME} --password-stdin"
-                sh "docker tag ${imageName} ${registryDomain}/${imageName}"
-                sh "docker push ${registryDomain}/${imageName}"
-            }
+    script {
+        withCredentials([usernamePassword(credentialsId: 'nexus', usernameVariable: 'NEXUS_USERNAME', passwordVariable: 'NEXUS_PASSWORD')]) {
+            sh "echo $NEXUS_PASSWORD | docker login ${registryDomain} -u ${NEXUS_USERNAME} --password-stdin"
+            sh "docker tag ${imageName} ${registryDomain}/${imageName}"
+            sh "docker push ${registryDomain}/${imageName}"
         }
     }
+    
 }
