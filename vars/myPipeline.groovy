3d6afc616e26902b4b60519c1aa945f41887fe40
Michalis Toutoudakis <miketoutou@gmail.com>
2024-09-16 13:29:54 +0300

    Update myPipeline.groovy

--- vars/myPipeline.groovy
+++ vars/myPipeline.groovy
@@ -6,28 +6,28 @@ def call(Map pipelineParams){
                 apiVersion: v1
                 kind: Pod
                 spec:
-                containers:
-                - name: maven
-                  image: maven:3.9.9-ibm-semeru-21-jammy
-                  command:
-                  - cat
-                  tty: true
-                - name: docker
-                  image: docker:24.0.2-dind
-                  command:
-                  - dockerd
-                  - --host=unix:///var/run/docker.sock
-                  - --insecure-registry=nexus-docker.nexus.svc.cluster.local:8083
-                  securityContext:
-                  privileged: true
-                - name: kubehelm
-                  image: 10.108.168.228:8083/kubehelm:1.0.0
-                  command:
-                  - cat
-                  tty: true
+                  containers:
+                  - name: maven
+                    image: maven:3.9.9-ibm-semeru-21-jammy
+                    command:
+                    - cat
+                    tty: true
+                  - name: docker
+                    image: docker:24.0.2-dind
+                    command:
+                    - dockerd
+                    - --host=unix:///var/run/docker.sock
+                    - --insecure-registry=nexus-docker.nexus.svc.cluster.local:8083
+                    securityContext:
+                    privileged: true
+                  - name: kubehelm
+                    image: 10.108.168.228:8083/kubehelm:1.0.0
+                    command:
+                    - cat
+                    tty: true
                 """
             }
-        }
+    }
         stages {
             stage('Build') {
                 steps {
